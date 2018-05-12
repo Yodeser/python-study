19da44cf81cee5bc64a23e548416ea9b806b92f5
Yodes Yang <yodeser@gmail.com>
2018-05-13 00:28:02 +0800

    Done exercise #0001

---
+++ src/0001_Generate_activeCode.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# //todo
+# Created 18:05, 2018/5/8 by Yodes Yang
+# About charset's history, and above bytes and str in python3: https://blog.csdn.net/lyb3b3b/article/details/74993327
+from numpy import random
+import hashlib
+
+size = 200
+ids = random.random_integers(10, 10000, size)
+ids = [str(i) for i in ids]
+print(ids)
+
+var = hashlib.md5()
+hashlib.md5()
+for code in range(0, len(ids)):
+    var.update(ids[code].encode("utf8"))
+    ids[code] = var.hexdigest()
+
+print(ids)
